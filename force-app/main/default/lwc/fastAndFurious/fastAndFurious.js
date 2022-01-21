d9703678eee5b7d1137aa9e6f5f27aedfc1c0fe8
Jason Xu <jsnx21@gmail.com>
2022-01-20 19:26:52 -0800

    yay soluiton found

--- force-app/main/default/lwc/fastAndFurious/fastAndFurious.js
+++ force-app/main/default/lwc/fastAndFurious/fastAndFurious.js
@@ -4,11 +4,19 @@ import getResponse from '@salesforce/apex/FastAndFuriousController.returnJSON';
 export default class App extends LightningElement {
   @track retrievedMovies;
   @track error;
+  @track movieData = [];
   @wire(getResponse)
   populateWithResponse({error, data}) {
     console.log('data : ' + data);
     console.log('error : ' + error);
     if (data) {
+      // unpack data here.
+      var finalMovieData = [];
+      for (var movieObj in data) {
+        for (var key in movieObj) {
+          this.movieData.push({value:movieObj[key], key:key});
+        }
+      }
       this.retrievedMovies = data;
       this.error = undefined;
     } else if (error) {
