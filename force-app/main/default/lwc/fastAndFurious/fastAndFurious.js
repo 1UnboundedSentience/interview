108bb90b2f0e1ad60d7922876fa4d05ffc9c2430
Jason Xu <jsnx21@gmail.com>
2022-01-20 20:01:53 -0800

    js clean up

--- force-app/main/default/lwc/fastAndFurious/fastAndFurious.js
+++ force-app/main/default/lwc/fastAndFurious/fastAndFurious.js
@@ -6,21 +6,8 @@ export default class App extends LightningElement {
   @track error;
   @wire(getResponse)
   populateWithResponse({error, data}) {
-    console.log('data : ' + data);
-    console.log('error : ' + error);
     if (data) {
-      // unpack data here.
-      var finalMovieData = [];
       for (let i = 0; i<data.length; i++) {
-       //  data[i] each object -         {
-       /*   "id": 1,
-          "title": "The Fast and the Furious",
-          "release_date": "2001-06-22T00:00:00Z",
-          "opening_revenue": "40,089,015"
-      },*/
-      console.log(i);
-      console.log(data[i].id);
-      console.log(data[i].title);
         this.retrievedMovies.push({
           "id" : data[i].id,
           "title" : data[i].title,
@@ -28,7 +15,6 @@ export default class App extends LightningElement {
           "opening_revenue" : data[i].opening_revenue,
         });
       }
-      console.log(this.MovieData);
       this.error = undefined;
     } else if (error) {
       this.retrievedMovies = undefined;
